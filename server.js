307fb4005b43d51146ab89e0971d57e261a39f64
sulhaSabn <95939206+sulhaSabn@users.noreply.github.com>
2026-05-25 19:15:30 +0430

    Update server.js

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>{
+.then(() => {
    console.log("MongoDB Connected");
 })
-.catch(err=>{
-   console.log(err);
+.catch(err => {
+   console.log("Mongo Error:", err);
 });
 
 app.use("/api/auth", authRoutes);
@@ -29,12 +29,12 @@ app.use("/api/payment", paymentRoutes);
 app.use("/api/leaderboard", leaderboardRoutes);
 app.use("/api/game", gameRoutes);
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
    res.send("GameFi 2048 Backend Running");
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
-   console.log("Server Running On Port", PORT);
+app.listen(PORT, () => {
+   console.log(`Server Running On Port ${PORT}`);
 });
