f9997e340bf16ca0698a57aed26d2ba91de4ba1a
sulhaSabn <95939206+sulhaSabn@users.noreply.github.com>
2026-05-25 20:23:38 +0430

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const paymentRoutes = require("./payment");
 const leaderboardRoutes = require("./leaderboard");
 const shopRoutes = require("./shop");
 const adsRoutes = require("./ads");
+const adminRoutes = require("./admin");
 
 const app = express();
 
@@ -17,18 +18,35 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log(err));
+.then(() => {
 
-app.use("/auth",authRoutes);
-app.use("/game",gameRoutes);
-app.use("/payment",paymentRoutes);
-app.use("/leaderboard",leaderboardRoutes);
-app.use("/shop",shopRoutes);
-app.use("/ads",adsRoutes);
+   console.log("MongoDB Connected");
+
+})
+.catch(err => {
+
+   console.log(err);
+
+});
+
+app.use("/auth", authRoutes);
+app.use("/game", gameRoutes);
+app.use("/payment", paymentRoutes);
+app.use("/leaderboard", leaderboardRoutes);
+app.use("/shop", shopRoutes);
+app.use("/ads", adsRoutes);
+app.use("/admin", adminRoutes);
 
 require("./walletWatcher");
 
-app.listen(process.env.PORT || 3000,()=>{
+app.get("/", (req, res) => {
+
+   res.send("2048 Backend Running");
+
+});
+
+app.listen(process.env.PORT || 3000, () => {
+
    console.log("Server Running");
+
 });
