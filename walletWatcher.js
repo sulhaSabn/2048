3e06a1fb7e347494391ddf3c6ffbd6800fb053c7
sulhaSabn <95939206+sulhaSabn@users.noreply.github.com>
2026-05-25 14:14:11 +0430

    Update walletWatcher.js

--- walletWatcher.js
+++ walletWatcher.js
@@ -1,17 +1,7 @@
 const cron = require("node-cron");
-const User = require("./User");
 
 cron.schedule("*/30 * * * * *", async()=>{
 
-   const users = await User.find({guest:false});
-
-   for(const user of users){
-
-      console.log("Checking wallet:", user.walletAddress);
-
-      // بررسی تراکنش USDT
-      // افزایش سکه خودکار
-
-   }
+   console.log("Wallet Watcher Running");
 
 });
