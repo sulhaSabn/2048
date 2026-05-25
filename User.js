410bf97b8c2100683154b9bfd63846de6fe5c0e3
sulhaSabn <95939206+sulhaSabn@users.noreply.github.com>
2026-05-25 19:16:03 +0430

    Update User.js

--- User.js
+++ User.js
@@ -2,11 +2,24 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
 
-   username: { type: String, required: true, unique: true },
-   password: { type: String, required: true },
+   username: {
+      type: String,
+      required: true,
+      unique: true
+   },
 
-   walletAddress: { type: String },
-   privateKey: { type: String },
+   password: {
+      type: String,
+      required: true
+   },
+
+   walletAddress: {
+      type: String
+   },
+
+   privateKey: {
+      type: String
+   },
 
    coins: {
       type: Number,
