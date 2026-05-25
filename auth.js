fb9481dce90ba4d4ace295b0089cdf0834cb1a7c
sulhaSabn <95939206+sulhaSabn@users.noreply.github.com>
2026-05-25 14:12:47 +0430

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const TronWeb = require("tronweb");
+
+const { TronWeb } = require("tronweb");
 
 const User = require("./User");
 
@@ -20,30 +21,110 @@ router.post("/register", async(req,res)=>{
       const exists = await User.findOne({username});
 
       if(exists){
+
          return res.json({
             success:false,
             message:"نام کاربری وجود دارد"
          });
+
       }
 
       const wallet = await tronWeb.createAccount();
 
-      const hashed = await bcrypt.hash(password,10);
+      const hashed =
+      await bcrypt.hash(password,10);
 
       const user = await User.create({
+
          username,
+
          password:hashed,
-         walletAddress:wallet.address.base58,
-         privateKey:wallet.privateKey
+
+         walletAddress:
+         wallet.address.base58,
+
+         privateKey:
+         wallet.privateKey
+
       });
 
       res.json({
+
          success:true,
+
          wallet:user.walletAddress,
+
          message:"ثبت نام موفق"
+
       });
 
    }catch(err){
+
+      console.log(err);
+
+      res.json({
+
+         success:false,
+
+         message:"خطا در ساخت کیف پول"
+
+      });
+
+   }
+
+});
+
+router.post("/login", async(req,res)=>{
+
+   try{
+
+      const {username,password} = req.body;
+
+      const user =
+      await User.findOne({username});
+
+      if(!user){
+
+         return res.json({
+            success:false,
+            message:"کاربر پیدا نشد"
+         });
+
+      }
+
+      const match =
+      await bcrypt.compare(
+         password,
+         user.password
+      );
+
+      if(!match){
+
+         return res.json({
+            success:false,
+            message:"رمز اشتباه است"
+         });
+
+      }
+
+      const token = jwt.sign(
+         {id:user._id},
+         process.env.JWT_SECRET
+      );
+
+      res.json({
+
+         success:true,
+
+         token,
+
+         user
+
+      });
+
+   }catch(err){
+
+      console.log(err);
 
       res.json({
          success:false,
@@ -51,55 +132,6 @@ router.post("/register", async(req,res)=>{
       });
 
    }
-
-});
-
-router.post("/guest", async(req,res)=>{
-
-   const guest = await User.create({
-      guest:true,
-      coins:20
-   });
-
-   res.json({
-      success:true,
-      user:guest
-   });
-
-});
-
-router.post("/login", async(req,res)=>{
-
-   const {username,password} = req.body;
-
-   const user = await User.findOne({username});
-
-   if(!user){
-      return res.json({
-         success:false,
-         message:"کاربر پیدا نشد"
-      });
-   }
-
-   const match = await bcrypt.compare(password,user.password);
-
-   if(!match){
-      return res.json({
-         success:false,
-         message:"رمز اشتباه است"
-      });
-   }
-
-   const token = jwt.sign(
-      {id:user._id},
-      process.env.JWT_SECRET
-   );
-
-   res.json({
-      success:true,
-      token,
-      user
-   });
 
 });
 
