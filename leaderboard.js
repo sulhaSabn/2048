4ca20d4a5c62528d06c6f2b08652af993e582d6f
sulhaSabn <95939206+sulhaSabn@users.noreply.github.com>
2026-05-25 19:19:20 +0430

    Modify leaderboard route to return top 20 users
    Updated leaderboard route to fetch top 20 users instead of 50, and added error handling.

--- leaderboard.js
+++ leaderboard.js
@@ -1,16 +1,25 @@
 const express = require("express");
+
 const User = require("./User");
 
 const router = express.Router();
 
-router.get("/", async(req,res)=>{
+router.get("/", async (req, res) => {
 
-   const users = await User
-   .find({guest:false})
-   .sort({bestScore:-1})
-   .limit(50);
+   try {
 
-   res.json(users);
+      const users = await User.find()
+      .sort({ bestScore: -1 })
+      .limit(20);
+
+      res.json(users);
+
+   } catch (err) {
+
+      console.log(err);
+
+      res.json([]);
+   }
 
 });
 
